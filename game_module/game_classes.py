27a22c01636daec64a5ada4df8c47e1e215b870a
= <=>
2024-10-29 14:29:14 -0700

    Add city data, game board classes

--- game_module/game_classes.py
+++ game_module/game_classes.py
@@ -1,12 +1,61 @@
 #%%
 import pandas as pd
-import numpy as np
 import random as rand
 
 ### pandas and numpy required
 
 ###pandas and numpy required
 ### card banks contain dataframes representing the deck, discard, and face up piles
+
+class city:
+    def __init__(self):
+        self.name = ""
+        self.harbor = False
+        self.owner = ""
+        self.connections = []
+
+class route:
+    def __init__(self,route_code):
+        self.route_code = route_code
+        self.city_list = []
+        self.color = ""
+        self.type = ""
+        self.cost = 0
+        self.length = 0
+        self.points = 0
+        self.owner = ""       
+
+class game_board:
+    def __init__(self):
+        self.name = ""
+        self.routes = []
+        self.cities = []
+
+    def initialize_board_from_data(self,city_csv,route_csv):
+        city_info = pd.read_csv(city_csv)
+        route_info = pd.read_csv(route_csv)
+
+        for city in city_info:
+            new_city = city()
+            new_city.name = city[1]["name"]
+            new_city.harbor = city[1]["harbor"]
+
+            self.cities.append(new_city)
+        
+        for route in route_info:
+            new_route = route(route[1]['route_code'])
+            new_route.city_list = [route[1]['city_a'],route[1]['city_b']]
+            new_route.color = route[1]['color']
+            new_route.type = route[1]['type']
+            new_route.cost = route[1]['cost']
+            new_route.length = route[1]['length']
+            new_route.points = route[1]['points']
+
+            ###appending city connections to each of the cities
+            [city for city in self.cities if city.name == route[1]['city_a']][0].connections.append(route[1]['city_b'])
+            [city for city in self.cities if city.name == route[1]['city_b']][0].connections.append(route[1]['city_a'])
+
+            self.routes.append(new_route)
 
 class card:
     def __init__(self):
@@ -15,182 +64,165 @@ class card:
         self.harbor = False
         self.value = 0
 
-class city:
-    def __init__(self):
-        self.name = ""
-        self.harbor = False
-        self.connections = []
-
-class game_board:
-    def __init__(self):
-        self.name = ""
-
-
 class deck:
     def __init__(self,name):
         self.name = name
-        self.cards = []
+        self.train_draw_pile = []
+        self.ship_draw_pile = []
+        self.faceup_pile = []
+        self.discard_pile = []
 
-    def reset_deck(self):
-        self.cards = []
+    def reset_discard_pile(self):
+        self.discard_pile = []
 
     def initialize_deck_from_data(self,card_csv):
         card_info = pd.read_csv(card_csv)
 
         for card_types in card_info.iterrows():
-            for x in range(0,card_types[1]['quantity']):
-                new_card = card()
-                new_card.color = card_types[1]['color']
-                new_card.type = card_types[1]['type']
-                new_card.harbor = card_types[1]['harbor']
-                new_card.value = card_types[1]['value']
+            for card_num in range(0,card_types[1]['quantity']):
+                    new_card = card()
+                    new_card.color = card_types[1]['color']
+                    new_card.type = card_types[1]['type']
+                    new_card.harbor = card_types[1]['harbor']
+                    new_card.value = card_types[1]['value']
 
-                self.cards.append(new_card)
+                    if new_card.type == 'train':
+                        self.train_draw_pile.append(new_card)
+                    elif new_card.type == 'ship':
+                        self.ship_draw_pile.append(new_card)
+
+    def initialize_faceup_pile(self):
+        for train_card in range(0,3):
+            self.transfer_one_card_from_draw_to_faceup_pile('train')
+
+        for ship_card in range(0,3):
+            self.transfer_one_card_from_draw_to_faceup_pile('ship') 
+
+    def deal_start_of_game_hand_to_player(self,player):
+        
+        ### deal 3 train cards to a player
+        train_cards = rand.sample(self.train_draw_pile,3)
+        player.hand.cards = player.hand.cards + train_cards
+        self.train_draw_pile = list(set(self.train_draw_pile) - set(train_cards))
+
+        ### deal 7 ship cards to a player
+        ship_cards = rand.sample(self.ship_draw_pile,7)
+        player.hand.cards = player.hand.cards + ship_cards
+        self.ship_draw_pile = list(set(self.ship_draw_pile) - set(ship_cards))
+
+        print(f'''{player.name} has been dealt a new hand''') 
 
     def output_deck_as_dataframe(self):
         output_df = pd.DataFrame(columns = ['color','type','harbor','value'])
 
-        for card in self.cards:
+        for card in self.train_draw_pile:
             new_card_df = pd.DataFrame([[card[1]['color'],card[1]['type'],card[1]['harbor'],card[1]['value']]],columns = ['color','type','harbor','value'])
 
             output_df = pd.concat([output_df,new_card_df])
 
+        for card in self.ship_draw_pile:
+            new_card_df = pd.DataFrame([[card[1]['color'],card[1]['type'],card[1]['harbor'],card[1]['value']]],columns = ['color','type','harbor','value'])
+
+            output_df = pd.concat([output_df,new_card_df])           
+
         return output_df
+    
+    def reset_draw_piles_from_discard(self):
+        self.train_draw_pile = self.train_draw_pile + [train_card for train_card in self.discard_pile if train_card.type == 'train']
+        self.ship_draw_pile = self.ship_draw_pile + [ship_card for ship_card in self.discard_pile if ship_card.type == 'ship']
+        self.reset_discard_pile()
 
+    def transfer_one_card_from_draw_to_faceup_pile(self,type):
 
-class hand(deck):
-    def __init__(self,deck):
-        self.cards = deck.cards
+        if (len(self.train_draw_pile) == 0) | (len(self.ship_draw_pile) == 0):
+                self.reset_draw_piles_from_discard()
 
-    def draw_cards_from_draw_deck(self,draw_deck,draw_size):
-        if len(draw_deck.cards) < draw_size:
-            raise ValueError("Not Enough Cards In Deck")
+        if type == 'train':
+            new_card = rand.sample(self.train_draw_pile,1)
+            self.faceup_pile = self.faceup_pile + new_card
+            self.train_draw_pile = list(set(self.train_draw_pile) - set(new_card))
+
+        elif type == 'ship':
+            new_card = rand.sample(self.ship_draw_pile,1)
+            self.faceup_pile = self.faceup_pile + new_card
+            self.ship_draw_pile = list(set(self.ship_draw_pile) - set(new_card))
+
+class hand():
+    def __init__(self):
+        self.cards = []
+
+    def draw_cards_from_draw_deck(self,deck,draw_size):
+        if len(deck.draw_pile) < draw_size:
+            deck.reset_deck_from_discard() 
         
-        new_cards = rand.sample(draw_deck.cards,draw_size)
+        new_cards = rand.sample(deck.draw_pile,draw_size)
 
         self.cards = self.cards + new_cards
-        draw_deck.cards = list(set(draw_deck.cards) - set(new_cards))
+        deck.draw_pile = list(set(deck.draw_pile) - set(new_cards))
 
-    def draw_card_from_face_up_pile(self,face_up_pile):
-        face_up_pile.cards
+    def choose_one_card_from_face_up_pile(self,deck,card,type):
+        self.cards = self.cards + card
+        deck.faceup_pile = list(set(deck.faceup_pile)-set(card))
+        deck.transfer_one_card_from_draw_to_faceup_pile(type)
 
-
-class draw_deck(deck):
-    def __init__(self,deck):
-        self.cards = deck.cards
-
-    def reset_deck_from_discard(self,discard_deck):
-        self.cards = self.cards + discard_deck.cards
-        discard_deck.reset_deck()
-
-class discard_deck(deck):
-    def __init__(self,deck):
-        self.cards = deck.cards
-
-class face_up_pile(deck):
-    def __init__(self,deck):
-        self.cards = deck.cards
-
-
-#%%
-class card_bank:
-    def __init__(self,name):
-        self.name = name
-        self.deck = pd.DataFrame(columns = ['color','type','harbor','value'])
-        self.discard_pile = pd.DataFrame(columns = ['color','type','harbor','value'])
-        self.face_up_pile = pd.DataFrame(columns = ['color','type','harbor','value'])
-
-    def reset_deck_from_discard(self):
-        self.deck = pd.concat([self.deck,self.discard_pile]).reset_index(drop = True)
-        self.discard_pile = pd.DataFrame(columns = ['color','type','harbor','value'])
+    def discard_from_hand(self,deck,hand_discard):
+        deck.discard_pile = deck.discard_pile + hand_discard
+        self.cards = list(set(self.cards) - set(hand_discard))
         
-        print("Deck has been reset from discard pile")
-
-    def draw_from_deck_to_face_up_pile(self):
-
-        draws = 6 - np.shape(self.face_up_pile)[0]
-
-        ### if the deck is not large enough to accomodate the number of draws, the deck must be reset from discard
-        if np.shape(self.deck)[0] < draws:
-            self.reset_deck_from_discard()
-    
-        draw_action = self.deck.sample(draws)
-
-        self.face_up_pile = pd.concat([self.face_up_pile,draw_action])
-        self.deck = self.deck.drop(list(draw_action.index)).reset_index(drop=True)
-        self.face_up_pile = self.face_up_pile.reset_index(drop = True)
+        print(f'''Number of cards discarded: {str(len(hand_discard))}''')
 
 class ticket:
     def __init__(self):
-        self.route_df = pd.DataFrame(columns = ['order','route'])
-        self.base_points = 0 
-        self.in_order_point_bonus = 0
-        self.status = "Incomplete"
-class route:
-    def __init__(self,route_code):
-        self.route_code = route_code
-        self.city_list = []
-        self.color = ""
-        self.type = ""
-        self.cost = 0
+        self.cities = []
         self.points = 0
+        self.status = "Incomplete"
 
-class player:
-    def __init__(self,name):
+class piece:
+    def __init__(self):
+        owner = ""
+        type = ""
+
+class player(hand):
+    def __init__(self,name,hand):
         self.name = name
-        self.hand = pd.DataFrame(columns = ['color','type','harbor','value'])
-        self.pieces = pd.DataFrame(columns = ['type'])
-        self.routes = pd.DataFrame(columns = ['city_list','color','cost','points'])
+        self.hand = hand
+        self.pieces = []
+        self.routes = []
 
-    ### actions
-    def deal_start_of_game_hand(self,card_bank):
-
-        train_cards = card_bank.deck[card_bank.deck['type'] == 'train'].sample(3)
-        ship_cards = card_bank.deck[card_bank.deck['type'] == 'ship'].sample(7)
-
-        new_cards = pd.concat([train_cards,ship_cards])
-        card_bank.deck = card_bank.deck.drop(list(new_cards.index)).reset_index(drop = True)
-        new_cards = new_cards.reset_index(drop=True)
-
-        self.hand = new_cards
-
-        print(f'''{self.name} has been dealt a new hand''')      
-    
-    def draw_from_deck_to_hand(self,card_bank,draws):
-    
-        ### if the deck is not large enough to accomodate the number of draws, the deck must be reset from discard
-        if np.shape(card_bank.deck)[0] < draws:
-            card_bank.reset_deck_from_discard()
-
-        new_cards = card_bank.deck.sample(draws)
-        card_bank.deck = card_bank.deck.drop(list(new_cards.index)).reset_index(drop = True)
-        new_cards = new_cards.reset_index(drop = True)
+    ### 20 trains and 40 ships is the recommended distribution
+    def choose_piece_distribution(self,trains = 20, ships = 40):
+        if trains <= 0 | ships <= 0:
+            raise ValueError(f'''Cannot have negative train ({trains}) or ship ({ships}) pieces. Reselect piece distribution.''')
         
-        self.hand = pd.concat([self.hand,new_cards]).reset_index(drop = True)
-
-        print("Cards drawn to hand: " + str(np.shape(new_cards)[0]))
-
-    def discard_from_hand(self,card_bank,hand_discard):
-        card_bank.discard_pile = pd.concat([card_bank.discard_pile,hand_discard]).reset_index(drop = True)
-        self.hand = self.hand.drop(list(hand_discard.index)).reset_index(drop = True)
+        elif trains >= 60 | ships >= 60:
+            raise ValueError(f'''Too many train ({trains}) or ship ({ships}) pieces. Reselect piece distribution.''')
         
-        print(f'''Number of cards discarded: {str(np.shape(hand_discard)[0])}''')
+        elif trains + ships != 60:
+            raise ValueError(f'''Train ({trains}) and ship ({ships}) pieces must total to 60 pieces. Current: {trains + ships} pieces. Reselect piece distribution.''')
 
+        for train in range(0,trains):
+            new_piece = piece()
+            new_piece.owner = self.name
+            new_piece.type = "train"
 
-    def draw_from_face_up_pile_to_hand(self,card_bank,face_up_index_list):
-        new_cards = card_bank.face_up_pile.iloc[[face_up_index_list]]
-        card_bank.face_up_pile = card_bank.face_up_pile.drop(list(new_cards.index)).reset_index(drop = True)
-        card_bank.draw_from_deck_to_face_up_pile()
-        self.hand = pd.concat([self.hand,new_cards]).reset_index(drop = True)
+            self.pieces.append(new_piece)
 
-        print(f'''Number of cards drawn from the face_up_pile: {str(np.shape(new_cards)[0])}''')
-        
+        for ship in range(0,ships):
+            new_piece = piece()
+            new_piece.owner = self.name
+            new_piece.type = "ship"
 
+            self.pieces.append(new_piece)
 
-class game:
-     def __init__(self,name):
-        self.name = name
-        self.bank = card_bank("game_bank")
+        for harbor in range(0,3):
+            new_piece = piece()
+            new_piece.owner = self.name
+            new_piece.type = "harbor"
+     
+class game(deck,game_board):
+     def __init__(self):
+        self.deck = deck
+        self.game_board = game_board
         self.player_list = []
 
     ### do we want to create a player dictionary that consists of the player's names?
@@ -227,49 +259,6 @@ class game:
 
             if number_of_players < 2 | number_of_players > 5:
                 raise ValueError(f'''Number of players has to be between 2 and 5. Current Number of Players: {number_of_players}''')
-
-            ### STANDARD GAME SCHEMA
-                ### There are 80 train cards, 11 of each color, except for 14 wilds
-                ### all wilds are harbor cards, 4/11 cards are harbor cards for each color
-                ### There are 60 ship cards, 24 are singles (4 of each color, except for wilds), 36 are doubles (6 of each color, except for wilds)
-                ### Each single ship card is a harbor card
-                #6 colors of trains x 2 harbor types
-                #1 wild card train type
-
-            route_list = [route]
-
-            
-            card_types = pd.DataFrame([['pink','train',False,1,7],
-                            ['pink','train',True,1,4],
-                            ['yellow','train',False,1,7],
-                            ['yellow','train',True,1,4],
-                            ['green','train',False,1,7],
-                            ['green','train',True,1,4],
-                            ['red','train',False,1,7],
-                            ['red','train',True,1,4],
-                            ['white','train',False,1,7],
-                            ['white','train',True,1,4],
-                            ['black','train',False,1,7],
-                            ['black','train',True,1,4],
-                            ['wild','train',False,1,14],
-                            ['pink','ship',True,1,4],
-                            ['pink','ship',False,2,6],
-                            ['yellow','ship',True,1,4],
-                            ['yellow','ship',False,2,6],
-                            ['green','ship',True,1,4],
-                            ['green','ship',False,2,6],
-                            ['red','ship',True,1,4],
-                            ['red','ship',False,2,6],
-                            ['white','ship',True,1,4],
-                            ['white','ship',False,2,6],
-                            ['black','ship',True,1,4],
-                            ['black','ship',False,2,6]],
-                                columns = ['color','type','harbor','value','quantity'])
-
-            for type in card_types.iterrows():
-                for x in range(0,type[1]['quantity']):
-                    new_card = pd.DataFrame([[type[1]['color'],type[1]['type'],type[1]['harbor'],type[1]['value']]],columns = ['color','type','harbor','value'])
-                    self.bank.deck = self.bank.deck.append(new_card).reset_index(drop = True)
 
             for x in range(1,(number_of_players + 1)):
                 new_player = player(f'''player_{x}''')
