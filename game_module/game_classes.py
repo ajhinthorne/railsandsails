17560ae617bacd1a4868bcca2ef7317555fdfa0f
= <=>
2024-11-12 11:14:24 -0800

    Fixing bugs

--- game_module/game_classes.py
+++ game_module/game_classes.py
@@ -36,10 +36,10 @@ class game_board:
         city_info = pd.read_csv(city_csv)
         route_info = pd.read_csv(route_csv)
 
-        for city in city_info:
+        for city_info in city_info:
             new_city = city()
-            new_city.name = city[1]["name"]
-            new_city.harbor = city[1]["harbor"]
+            new_city.name = city_info[1]["name"]
+            new_city.harbor = city_info[1]["harbor"]
 
             self.cities.append(new_city)
         
